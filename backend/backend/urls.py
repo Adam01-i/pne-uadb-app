dfc95c2ecca270f4232c8605931d096ebdf8916c
Adama Seck <seckmote@gmail.com>
2026-04-07 05:36:15 +0000

    creation des autres endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import (
     EtudiantViewSet, ClasseViewSet,
     AgentScolariteViewSet, BibliothecaireViewSet, MedecinViewSet
@@ -18,4 +19,7 @@ router.register(r'medecins', MedecinViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    # Endpoints JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
