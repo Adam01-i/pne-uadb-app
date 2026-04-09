8b75f2fac2da0a7df7eaab66c2175b97408bb910
Adama Seck <seckmote@gmail.com>
2026-04-09 04:18:44 +0000

    système de permissions + filtrage par rôle

---
+++ backend/users/permissions.py
@@ -0,0 +1,7 @@
+# pne-uadb-app/backend/users/permissions.py
+
+from rest_framework.permissions import BasePermission
+
+class IsAgent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == 'agent'
