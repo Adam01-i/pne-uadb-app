38181b41793986dc7787fbd9ee57f4d972fff5e0
ArameCisse <aramebacarcisse92@gmail.com>
2026-04-11 01:46:12 +0000

    feat: ajout app services (VisiteMedicale + ValidationBibliotheque)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'services',
     'rest_framework',
     'rest_framework_simplejwt',
     'inscriptions',
